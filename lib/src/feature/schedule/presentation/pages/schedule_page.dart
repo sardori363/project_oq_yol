d306c5521928fc0ce47d7e18a8805e97ded95189
sardorbek <odiljonovsardorbek98@gmail.com>
2024-07-10 11:55:03 +0500

    Sardorbek, 11-commit, schedule page

--- lib/src/feature/schedule/presentation/pages/schedule_page.dart
+++ lib/src/feature/schedule/presentation/pages/schedule_page.dart
@@ -1,22 +1,30 @@
 import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
+
+import "../../../../common/utils/extensions/context_extensions.dart";
 
 class SchedulePage extends StatelessWidget {
   const SchedulePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ColorScheme contextColor = context.theme.colorScheme;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(
-          "Schedule Page",
+          "Qatnovlar",
           style: Theme.of(context)
               .textTheme
               .titleMedium
               ?.copyWith(
-              fontWeight: FontWeight.w700,
-              fontSize: 18, fontFamily: "Poppins"),
+              fontWeight: FontWeight.w600,
+              fontSize: 22, fontFamily: "Poppins"),
         ),
+      ),
+      body: Column(
+        children: [
+        ],
       ),
     );
   }
